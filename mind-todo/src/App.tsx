aeaff915799ebe48bc0dd8cc931df9af8768d9eb
nine <chindoan72@gmail.com>
2025-12-17 09:59:14 +0700

    update

--- mind-todo/src/App.tsx
+++ mind-todo/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoPage from "./pages/Todo/TodoPage";
 import LoginPage from "./pages/Login/LoginPage";
-
 function App() {
   return  (
   <BrowserRouter>
